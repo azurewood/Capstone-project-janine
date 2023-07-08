40ae88ac04e537661b60cee4ce39b88674b5a882
JanineSooThow <j9soothow@gmail.com>
2023-07-08 15:56:39 +1200

    add files

---
+++ server/server.js
@@ -0,0 +1,24 @@
+//npm init 
+//npm install express
+//npm install dotenv (Way to read in configuration for application)
+//npm install mongoose --save (an object data modelling library - provides simple and structured way to work with MongoDB)
+
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+//importing dbConnect.js into our server.js file 
+let dbConnect = require("./dbConnect");
+
+// parse requests of content-type -application/json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my MongoDB application." });
+});
+
+// set port, listen for requests. Defining PORT as a variable 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
