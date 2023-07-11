311cb6e10475a5375e325820d3ff6065ec44f200
JanineSooThow <j9soothow@gmail.com>
2023-07-11 17:58:10 +1200

    update

--- frontend/fuelstaff/app/page.tsx
+++ frontend/fuelstaff/app/page.tsx
@@ -1,2 +1,17 @@
 //Homepage for StaffFuel
 'use client'
+import React from 'react';
+import LoginForm from '../src/components/LoginForm';
+
+const LandingPage: React.FC = () => {
+  return (
+    <>
+    <div>
+        <LoginForm></LoginForm>
+    </div>
+    </>
+  );
+};
+
+export default LandingPage;
+   
