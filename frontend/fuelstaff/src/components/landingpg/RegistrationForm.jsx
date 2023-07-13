ee1e78ec7d0ac90f17271e0edcbcf1f8b8eec693
JanineSooThow <j9soothow@gmail.com>
2023-07-13 16:50:28 +1200

    update

--- frontend/fuelstaff/src/components/landingpg/RegistrationForm.jsx
+++ frontend/fuelstaff/src/components/landingpg/RegistrationForm.jsx
@@ -1,6 +1,7 @@
 //Modal Sign In form 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+//importing necessary dependencies
+import { useState } from "react"; 
+import { useRouter } from "next/navigation"; //imported from Next.js to provide routing capabilities
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
@@ -8,29 +9,37 @@ import Modal from 'react-bootstrap/Modal';
 
 //declaring functional component LoginForm
 //state variable is used to control whether modal shows or not
+//component RegistrationForm takes two props: 'show' and 'closeSignUpForm'
 function RegistrationForm({show, closeSignUpForm}) {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [employeeId, setEmployeeId] = useState('')
   const [emailId, setEmailId] = useState('')
   const [password, setPassword] = useState('')
+//Access the router object - the 'useRouter' hook is used to access the router object, which allows for navigation after successful registration. 
   const router = useRouter()
 
+//Server URL is the URL where the registration data will be sent via a POST request
   const SERVER_URL = 'http://localhost:8080/auth/registration'
 
+//'userData' is an object that holds the form data entered by the user.
   const userData = {
     firstName, lastName, employeeId, emailId, password
   }
 
+//Function to handle form submission. The 'handleSubmit' function is called when the form is submitted.
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    
+
+//Send a POST request to the server with user data 
     const response = await fetch(SERVER_URL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify(userData)
     })
 
+//Handling the response
     if (response.ok) {
         const data = await response.json();
         console.log('USER REGISTERED!', data)
@@ -52,7 +61,9 @@ function RegistrationForm({show, closeSignUpForm}) {
           <Modal.Title>Register</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+           {/* The registration form */}
           <Form onSubmit={handleSubmit}>
+           {/* Form inputs */}
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>First Name </Form.Label>
               <Form.Control
@@ -60,6 +71,7 @@ function RegistrationForm({show, closeSignUpForm}) {
                 type="text"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First Name"
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -69,6 +81,7 @@ function RegistrationForm({show, closeSignUpForm}) {
                 type="text"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last Name"
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -78,8 +91,7 @@ function RegistrationForm({show, closeSignUpForm}) {
                 type="text"
                 value={employeeId}
                 onChange={(e) => setEmployeeId(e.target.value)}
-                placeholder="enter your 4-digit number"
-                
+                placeholder="Enter your 4-digit number"    
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -88,7 +100,7 @@ function RegistrationForm({show, closeSignUpForm}) {
                 autoFocus
                 type="email"
                 value={emailId}
-                placeholder="name@example.com"
+                placeholder="Email"
                 onChange={(e) => setEmailId(e.target.value)} 
               />
             </Form.Group>
@@ -97,12 +109,20 @@ function RegistrationForm({show, closeSignUpForm}) {
               controlId="passwordarea"
             >
               <Form.Label>Create Password</Form.Label>
-              <Form.Control type='password' rows={1} value={password} onChange={(e) => setPassword(e.target.value)} />
+              <Form.Control 
+              type='password' 
+              rows={1} 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder = "Password"
+               />
             </Form.Group>
             <Modal.Footer>
+          {/* Close button */}
           <Button variant="secondary" onClick={closeSignUpForm}>
             Close
           </Button>
+          {/* Submit button */}
           <Button variant="dark" type='submit'>
             Submit
           </Button>
