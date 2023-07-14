7ba2ab0cb72f6830206bbb417385bc947580c1c9
JanineSooThow <j9soothow@gmail.com>
2023-07-15 10:19:53 +1200

    update

---
+++ frontend/fuelstaff/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react';
+import CustomCarousel from '../../src/components/Carousel'
+
+
+export default function Dashboard() {
+    return (
+    <div>
+        <h1>hello world </h1>
+        <CustomCarousel/>
+    </div>
+    )
+}
