b778224764207d96183058e7d9171ccabde139ac
JanineSooThow <j9soothow@gmail.com>
2023-07-18 21:47:45 +1200

    corrected error

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const paymentController = require ('../controllers/paymentController.js');
 
 //never use GET when sending sensitive information
+// 'addPayment and getPaymentById' came from paymentController.js
 
-router.post('/payments', paymentController.createPayment);
+router.post('/payments', paymentController.addPayment);
 router.get('/payments/:paymentId', paymentController.getPaymentById);
 
 module.exports = router;
