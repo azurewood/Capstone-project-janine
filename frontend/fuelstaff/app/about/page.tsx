7200c570a5b1f0b5d9d31a60d4ce86a7f7d979d9
JanineSooThow <j9soothow@gmail.com>
2023-07-25 17:51:26 +1200

    update

--- frontend/fuelstaff/app/about/page.tsx
+++ frontend/fuelstaff/app/about/page.tsx
@@ -30,7 +30,7 @@ const AboutBox: React. FC = () => {
                     amidst short breaks and long cafeteria queues, Janine set out to develop a solution that would not only enhance the well-being of the staff but also positively impact patient care and outcomes. 
                     With StaffFuel, hospital staff can now enjoy a wide range of healthy food choices conveniently delivered to them, eliminating the need to waste precious break time waiting in line. By encouraging 
                     nutritious eating habits, this app strives to improve the overall health, performance, and focus of hospital staff, ultimately resulting in a more productive and efficient workplace.
-                    Recognizing the demanding nature of their job and the potential health risks that may arise from an unhealthy diet, StaffF provides easy access to balanced and wholesome meal options, reducing the 
+                    Recognizing the demanding nature of their job and the potential health risks that may arise from an unhealthy diet, StaffFuel provides easy access to balanced and wholesome meal options, reducing the 
                     risk of chronic diseases such as diabetes and heart conditions. The app's user-friendly interface offers staff members the convenience of selecting from a variety of healthy recipes, effectively saving 
                     them time and effort in meal planning and preparation.
                   
